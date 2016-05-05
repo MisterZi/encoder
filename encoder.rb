a41314b5df1e272261c20c2edd07a505517c0bd0
ale8270 <ale8270@yandex.ru>
2016-05-05 12:58:14 +0300

    add sha2 algoritm

--- encoder.rb
+++ encoder.rb
@@ -4,7 +4,7 @@ require 'digest'
 puts 'Введите слово или фразу для шифрования:'
 input = STDIN.gets.chomp
 
-puts "Каким способом зашифровать:\n1. MD5\n2. SHA1"
+puts "Каким способом зашифровать:\n1. MD5\n2. SHA1\n3. SHA2"
 encode_mode = STDIN.gets.to_i
 
 case encode_mode
@@ -12,4 +12,6 @@ case encode_mode
     puts Digest::MD5.hexdigest(input)
   when 2
     puts Digest::SHA1.hexdigest(input)
+  when 3
+    puts Digest::SHA2.hexdigest(input)
 end
