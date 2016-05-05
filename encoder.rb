d1a0c39016e893631a526d16d0eaaf15c6c96156
ale8270 <ale8270@yandex.ru>
2016-05-05 12:40:09 +0300

    release version 1.0

---
+++ encoder.rb
@@ -0,0 +1,15 @@
+# This module provides a framework for message digest libraries.
+require 'digest'
+
+puts 'Введите слово или фразу для шифрования:'
+input = STDIN.gets.chomp
+
+puts "Каким способом зашифровать:\n1. MD5\n2. SHA1"
+encode_mode = STDIN.gets.to_i
+
+case encode_mode
+  when 1
+    puts Digest::MD5.hexdigest(input)
+  when 2
+    puts Digest::SHA1.hexdigest(input)
+end
